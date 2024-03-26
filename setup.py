576c685b92ecddaee1088545f5f96d9241b0b7f2
Matthew Shen <mds377@cornell.edu>
2024-03-26 17:11:58 -0400

    add ccds template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Raincatcher is a groundbreaking new feature that lets you design with nature as your partner. Raincatcher lets property managers, architects, and planners see how their sites handle storms. First, small scale flood simulations are conducted based on your site's properties. Raincatcher then lets you see how nature based solutions like rain gardens and green roofs improve your properties ability to resist flooding. Finally, raincatcher provides the user data to make an informed decision on how they best build their site.',
+    author='Cornell Tech Solution Squad',
+    license='MIT',
+)
